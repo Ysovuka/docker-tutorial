b1ae4e53264c39e6f9bd6bc2f4b1858347c0b700
Joshua Thompson <ysovuka@gmail.com>
2017-08-03 22:01:20 -0500

    Fixed docker-build-run.cmd file.

--- docker-build-run.cmd
+++ docker-build-run.cmd
@@ -1,6 +1,7 @@
+powershell docker rmi docker-tutorial-build -f
 powershell docker build -f dockerfile.build -t docker-tutorial-build .
-powershell docker rm docker-tutorial
+powershell docker rm docker-tutorial -f
 powershell docker create --name docker-tutorial docker-tutorial-build
 powershell docker cp docker-tutorial:/publish ./
 powershell docker build -t docker-tutorial .
-powershell docker run -it --rm -p 5001:80 docker-tutorial
+powershell docker run -it --rm -d -p 5001:80 docker-tutorial -name docker-tutorial
